9c7d22c1bc017de9a01dbc506b8e92c62b5d0837
Oscar Davies <odavies@etherlive.co.uk>
2021-09-06 11:07:01 +0100

    Steal Code from Login-Interface

---
+++ Management/TrackedIdentity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hire_Hop_Interface.Management
+{
+    public class TrackedIdentity
+    {
+        #region Fields
+
+        public ClientConnection clientConnection = new ClientConnection();
+        public User user;
+
+        private string identity_str;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public TrackedIdentity(string identity)
+        {
+            identity_str = identity;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public void Update()
+        {
+        }
+
+        #endregion Methods
+    }
+
+    public class User
+    {
+        #region Properties
+
+        public string username { get; set; }
+
+        #endregion Properties
+    }
+}
