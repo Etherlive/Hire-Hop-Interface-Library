34b7ec0bcaf022e4daed8ad07b776b6ed6aae8dc
Oscar Davies <odavies@etherlive.co.uk>
2021-09-07 09:13:46 +0100

    Fetch a Job & Bool from Login rquest

--- Requests/Authentication.cs
+++ Requests/Authentication.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using Hire_Hop_Interface.Management;
 
 namespace Hire_Hop_Interface.Requests
@@ -8,7 +9,7 @@ namespace Hire_Hop_Interface.Requests
     {
         #region Methods
 
-        public static async Task<ClientConnection> Login(ClientConnection client, string username, string password, string companyId = "ELTH")
+        public static async Task<bool> Login(ClientConnection client, string username, string password, string companyId = "ELTH")
         {
             client = await RequestInterface.SendRequest(client, "login.php", contentList: new List<string>()
             {
@@ -22,7 +23,7 @@ namespace Hire_Hop_Interface.Requests
 
             var cookie = client.cookies;
 
-            return client;
+            return cookie.Count > 0 && cookie.Any(x=>x.Name=="id") && cookie["id"].Value != "deleted";
         }
 
         #endregion Methods
---
+++ Requests/Jobs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hire_Hop_Interface.Management;
+using Newtonsoft.Json.Linq;
+
+namespace Hire_Hop_Interface.Requests
+{
+    public static class Jobs
+    {
+        public static async Task<JObject> GetJobData(ClientConnection client, string jobId)
+        {
+            client = await RequestInterface.SendRequest(client, "php_functions/job_refresh.php", contentList: new List<string>()
+            {
+                $"job={jobId}"
+            });
+            string job_data = await client.__lastResponse.Content.ReadAsStringAsync();
+            try
+            {
+                return JObject.Parse(job_data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
