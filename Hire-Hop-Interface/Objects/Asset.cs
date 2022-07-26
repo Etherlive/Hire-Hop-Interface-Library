d86039c86881b311ef875c26f537c1724ac3d88b
Oscar Davies <oscar.william.davies@outlook.com>
2022-07-26 17:01:30 +0100

    Search For All Assets

--- Hire-Hop-Interface/Objects/Asset.cs
+++ Hire-Hop-Interface/Objects/Asset.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using Hire_Hop_Interface.Interface.Caching;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Hire_Hop_Interface.Objects
 {
@@ -15,9 +19,47 @@ namespace Hire_Hop_Interface.Objects
 
         #region Methods
 
-        public static async Task<Asset[]> Search()
+        public static async Task<SearchCollection<Asset>> SearchForAll(Interface.Connections.CookieConnection cookie)
         {
-            return null;
+            var req = new CacheableRequest("modules/stock/list.php", "GET", cookie);
+
+            req.AddOrSetQuery("head", "0");
+            req.AddOrSetQuery("del", "0");
+            req.AddOrSetQuery("_search", "false");
+            req.AddOrSetQuery("rows", "50");
+            req.AddOrSetQuery("nd", "1646206198206");
+            req.AddOrSetQuery("sidx", "TITLE");
+            req.AddOrSetQuery("sord", "asc");
+            req.AddOrSetQuery("local", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            int page = 1, lastCount = 0, lastPage = 100;
+            List<Asset> results = new List<Asset>();
+
+            while ((lastCount != results.Count && lastPage>=page) || page == 1)
+            {
+                lastCount = results.Count;
+                req.AddOrSetQuery("page", page.ToString());
+                var res = await req.ExecuteWithCache();
+
+                if (res.TryParseJson(out JsonElement? json))
+                {
+                    lastPage = json.Value.GetProperty("total").GetInt32();
+                    if (json.Value.TryGetProperty("rows", out var r))
+                    {
+                        var rows = r.EnumerateArray();
+                        while (rows.MoveNext())
+                        {
+                            results.Add(new Asset() { json = rows.Current });
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                page++;
+            }
+            return new SearchCollection<Asset>() { results = results.ToArray(), max_page = page };
         }
 
         #endregion Methods
---
+++ Test/AssetChecks.cs
@@ -0,0 +1,38 @@
+﻿using Hire_Hop_Interface.Interface;
+using Hire_Hop_Interface.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class AssetChecks
+    {
+        private Hire_Hop_Interface.Interface.Connections.CookieConnection cookie = new Hire_Hop_Interface.Interface.Connections.CookieConnection();
+        [TestMethod]
+        public void EnsureCookieManagerWorks()
+        {
+            var t_obj = Asset.SearchForAll(cookie);
+
+            t_obj.Wait();
+
+            var obj = t_obj.Result;
+
+            Assert.IsTrue(obj.results.Length > 0);
+        }
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            var req = Authentication.Login(cookie, Details.hh_email, Details.hh_password);
+
+            req.Wait();
+
+            Assert.IsTrue(req.Result);
+        }
+    }
+}
